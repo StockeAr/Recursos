7b276e1beccf4b33415cb589cad476dee8a9dd4e
StockeAr <proyecto.pp3@gmail.com>
2020-10-16 13:06:39 -0300

    prueba

--- server/build/controllers/pruebaControllers.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class PruebaController {
-    index(req, res) {
-        res.send('prueba');
-    }
-}
-const pruebacontroller = new PruebaController();
-exports.default = pruebacontroller;
--- server/src/controllers/indexController.ts
+++
@@ -1,8 +0,0 @@
-import {Request,Response} from 'express';
-
-class IndexController{
-    public index (req:Request,res:Response){
-        res.send('hello')
-    }
-}
-export const indexController=new IndexController();
